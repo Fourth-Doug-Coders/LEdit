c8f98ab0acdba97db5fa684a601523fb7b62a3b6
Yi Li <yl2326@cornell.edu>
2013-04-03 00:44:00 -0400

    josh didnt merge because of this line. also had to go to bed before i could take out this comment and request for a merge. hence, i merged.

--- src/document_view.h
+++ src/document_view.h
@@ -10,7 +10,6 @@ protected:
 public:
 	DocumentView();
 	DocumentView(int start_x, int start_y, int x_size, int y_size);
-	//waiting on Josh
 	virtual void display() = 0;
 	virtual bool resize(int start_x, int start_y, int x_size, int y_size);
 	virtual bool reposition(int start_x, int start_y);
